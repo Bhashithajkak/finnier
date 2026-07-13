e4f41800e079bf62e643e8a1bff343a7aee95da7
Bhashithajkak <kavindubhashitha2000@gmail.com>
2026-07-13 22:09:30 +0530

    Initial Commit

---
+++ src/main/java/com/example/finnier/FinnierApplication.java
@@ -0,0 +1,13 @@
+package com.example.finnier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinnierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinnierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/finnier/FinnierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finnier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinnierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
